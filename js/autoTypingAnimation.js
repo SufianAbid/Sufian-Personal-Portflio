8f636776a871c30b930f83afc426aab620704cda
Sufian Abid <sufianabid388@gmail.com>
2023-11-13 03:26:51 -0800

    First Commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        <span>Hi, I am Sufian Abid </span>],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
